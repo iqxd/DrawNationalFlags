d73c66f3531760f8e0d4e1f98e336481b0c3fe64
iqxd <327614680@qq.com>
2021-02-13 22:47:06 +0800

    update flagdraw

--- flagdraw.py
+++ flagdraw.py
@@ -1,6 +1,6 @@
-from turtle import _Screen, RawTurtle, Screen, mainloop
+from turtle import _Screen, RawTurtle, Screen, mainloop,Vec2D
 import math
-from typing import Union
+from typing import Union,Optional
 
 
 class Canvas:
@@ -35,9 +35,98 @@ class Canvas:
         self.painter = painter
         self.animate = animate
 
-    def finish(self) -> None:
+    def move(self,draw=None,*actions):
+        """
+        action type (
+        draw :  None pepup, (color,None) nofill, color | (color,color) fill
+        coordinate : Vec2D , use goto function
+        turndegree: tuple[str, int | float] , turn 'd' degree or 'r' radian , positive right, negative left
+        distance : int | float, positive forward , negative backward )
+        """
+        if draw:
+            if isinstance(draw,tuple) and draw[1] is None:
+                t = self.guider
+                t.color(draw[0])
+            else:
+                t = self.painter 
+                t.color(draw)
+        else:
+            t = self.guider
+        
+        if t==self.painter:
+            t.begin_fill()
+
+        for a in actions:
+            if isinstance(a,tuple):
+                if a[0] == 'd':
+                    if a[1]>=0:
+                        t.right(a[1])
+                    else:
+                        t.left(a[1])
+                elif a[0] == 'r':
+                    ...
+                else:
+                    t.goto(a)
+            else:
+                if a>=0:
+                    t.forward(a)
+                else:
+                    t.backward(-a)
+
+        if t == self.painter:
+            t.end_fill()
+                    
+
+    def line(self,start:Vec2D,rotate_or_end:Union[int,float,Vec2D],length:Union[int,float,None]=None):
+        t = self.guider
+        t.penup()
+        t.goto(start)
+        t.pendown()
+        if isinstance(rotate_or_end,int) or isinstance(rotate_or_end,float):
+            t.setheading(rotate_or_end)
+            t.forward(length)
+        else:
+            t.goto(rotate_or_end)
+        
+
+    def rect(self,center:Vec2D,
+            width:Union[int,float],
+            height:Union[int,float],
+            rotate:Union[int,float],
+            fillcolor:Optional[str]=None)->None:
+        t = self.painter if fillcolor else self.guider
+        if fillcolor:
+            t.color(fillcolor)
+            t.begin_fill()
+        t.penup()
+        t.goto(center)
+        t.setheading(rotate)
+        t.forward(width/2)
+        t.pendown()
+        t.right(90)
+        t.forward(height/2)
+        t.right(90)
+        t.forward(width)
+        t.right(90)
+        t.forward(height)
+        t.right(90)
+        t.forward(width)
+        t.right(90)
+        t.forward(height/2)
+        if fillcolor:
+            t.end_fill()
+
+    def hideguider(self):
         self.guider.hideturtle()
+
+    def hidepainter(self):
         self.painter.hideturtle()
+
+    def finish(self) -> None:
+        self.hidepainter()
+        for _ in range(self.guider.undobufferentries()):
+            self.guider.undo()
+        self.hideguider()
         if not self.animate:
             self.screen.update()
         mainloop()
@@ -45,6 +134,6 @@ class Canvas:
 
 if __name__ == '__main__':
     f = Canvas('test', 500, 500)
-    f.guider.circle(100)
-    f.painter.forward(200)
+    f.rect((50,50),200,150,10,'cyan')
+    f.move('red',(0,0),(50,0),('d',45),100)
     f.finish()
